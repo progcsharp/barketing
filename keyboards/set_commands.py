eecf68932cfb469430c5f625be13050060cf2717
unknown <fonror@mail.ru>
2025-04-04 19:11:04 +0300

    add menu command

--- keyboards/set_commands.py
+++ keyboards/set_commands.py
@@ -4,8 +4,6 @@ from aiogram.types import BotCommand
 async def set_commands(bot):
     commands = [
         BotCommand(command="/start", description="Запустить бота!"),
-        BotCommand(command="/report_issue", description="Сообщить о проблеме."),
-        BotCommand(command="/suggest", description="Предложить улучшение."),
-        BotCommand(command="/open", description="Открыть приложение.")
+        BotCommand(command="/about", description="Информация о компании"),
     ]
     await bot.set_my_commands(commands)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import bot
-# from keyboards.set_commands import set_commands
+from keyboards.set_commands import set_commands
 from register_handler.reg_callback import register_handlers_callback
 from register_handler.reg_commands import register_handlers_commands
 from register_handler.reg_message import register_handlers_message
@@ -14,7 +14,7 @@ async def main():
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
 
-    # await set_commands(bot)
+    await set_commands(bot)
 
     await register_handlers_commands(dp)
     await register_handlers_callback(dp)
